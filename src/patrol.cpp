356cc489e9f28f41dfc9b70344654a1aeffb21f2
robonetic <robonetic@tutamail.com>
2025-05-01 03:16:55 +0000

    Completed simulation of the turtlebot - fixed critical mistake in the last commit after trying to my refactor my code

--- src/patrol.cpp
+++ src/patrol.cpp
@@ -1,3 +1,4 @@
+#include "rclcpp/logging.hpp"
 #include <chrono>
 #include <cmath>
 #include <functional>
@@ -19,24 +20,17 @@ public:
         "scan", 2, bind(&Patrol::laser_scan_callback, this, _1));
     publisher_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
     timer_ = create_wall_timer(100ms, bind(&Patrol::control_loop, this));
+
+    RCLCPP_INFO(get_logger(), "Robot initialized - Ready for patrol!");
   }
 
 private:
-  void laser_scan_callback(const sensor_msgs::msg::LaserScan &msg) {
-    front_range = (M_PI / 2) / msg.angle_increment;
-    front_ray = (0 - msg.angle_min) / msg.angle_increment;
-    front_ray_view = (35.0 * M_PI / 180.0) / msg.angle_increment;
-  }
+  void laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+    lasser_scan_msg = msg;
 
-  bool is_obstacle_in_front() {
-    for (int i = front_ray - front_ray_view; i < front_ray + front_ray_view;
-         i++) {
-      if (scan_msg.ranges[i] < 0.35) {
-        return true;
-      }
-    }
-
-    return false;
+    front_range = (M_PI / 2) / msg->angle_increment;
+    front_ray = (0 - msg->angle_min) / msg->angle_increment;
+    front_ray_view = (35.0 * M_PI / 180.0) / msg->angle_increment;
   }
 
   float calculate_safe_direction() {
@@ -44,20 +38,34 @@ private:
     float largest_distance_ray = 0.0;
 
     for (int i = front_ray - front_range; i <= front_ray + front_range; i++) {
-      if (isinf(scan_msg.ranges[i])) {
+      if (isinf(lasser_scan_msg->ranges[i])) {
         continue;
       }
 
-      if (scan_msg.ranges[i] > largest_distance_ray) {
-        largest_distance_ray = scan_msg.ranges[i];
+      if (lasser_scan_msg->ranges[i] > largest_distance_ray) {
+        largest_distance_ray = lasser_scan_msg->ranges[i];
         largest_distance_ray_idx = i;
       }
     }
 
-    float direction_ = (scan_msg.angle_min +
-                        largest_distance_ray_idx * scan_msg.angle_increment);
+    float direction_ =
+        (lasser_scan_msg->angle_min +
+         largest_distance_ray_idx * lasser_scan_msg->angle_increment);
 
     return direction_;
+  }
+
+  bool is_obstacle_in_front() {
+    for (int i = front_ray - front_ray_view; i < front_ray + front_ray_view;
+         i++) {
+      if (lasser_scan_msg->ranges[i] < 0.35) {
+        RCLCPP_INFO(get_logger(),
+                    "Obstacle detected - Calucating greatest distance...");
+        return true;
+      }
+    }
+
+    return false;
   }
 
   void control_loop() {
@@ -66,6 +74,9 @@ private:
 
     if (is_obstacle_in_front()) {
       cmd_vel_msg.angular.z = calculate_safe_direction() / 2;
+      RCLCPP_INFO(get_logger(),
+                  "Calculated Safested direction - Rotating at angular z: %f",
+                  cmd_vel_msg.angular.z);
     }
 
     publisher_->publish(cmd_vel_msg);
@@ -76,7 +87,8 @@ private:
   int front_ray_view;
 
   geometry_msgs::msg::Twist cmd_vel_msg;
-  sensor_msgs::msg::LaserScan scan_msg;
+  sensor_msgs::msg::LaserScan::SharedPtr lasser_scan_msg;
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscriber_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
