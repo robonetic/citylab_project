2a99a52974bb7303af2215593c6cdc9d6965ab6d
robonetic <robonetic@tutamail.com>
2025-05-01 02:37:57 +0000

    Improved code quality a bit, moved core logic to sperate functions and made the laser scan callback more efficient

---
+++ build/robot_patrol/CMakeFiles/patrol_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/patrol_node.dir/src/patrol.cpp.o"
+  "CMakeFiles/patrol_node.dir/src/patrol.cpp.o.d"
+  "patrol_node"
+  "patrol_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/patrol_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/patrol.cpp
+++ src/patrol.cpp
@@ -1,12 +1,9 @@
-#include "rclcpp/logging.hpp"
 #include <chrono>
 #include <cmath>
 #include <functional>
 #include <geometry_msgs/msg/twist.hpp>
 #include <iostream>
-#include <iterator>
 #include <math.h>
-#include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <unistd.h>
@@ -26,48 +23,57 @@ public:
 
 private:
   void laser_scan_callback(const sensor_msgs::msg::LaserScan &msg) {
-    scan_msg = msg;
+    front_range = (M_PI / 2) / msg.angle_increment;
+    front_ray = (0 - msg.angle_min) / msg.angle_increment;
+    front_ray_view = (35.0 * M_PI / 180.0) / msg.angle_increment;
   }
 
-  void control_loop() {
-    int front_ray = (0 - scan_msg.angle_min) / scan_msg.angle_increment;
-    int front_ray_view = (20.0 * M_PI / 180.0) / scan_msg.angle_increment;
-    int front_range = (M_PI / 2) / scan_msg.angle_increment;
-    int largest_distance_ray_idx = 0;
-    float largest_distance_ray = 0.0;
-    bool obstacle_detected = false;
-
-    cmd_vel_msg.linear.x = 0.1;
-    cmd_vel_msg.angular.z = 0.0;
-
+  bool is_obstacle_in_front() {
     for (int i = front_ray - front_ray_view; i < front_ray + front_ray_view;
          i++) {
       if (scan_msg.ranges[i] < 0.35) {
-
-        obstacle_detected = true;
+        return true;
       }
     }
 
-    if (obstacle_detected) {
-      for (int ray = front_ray - front_range; ray <= front_ray + front_range;
-           ray++) {
-        if (isinf(scan_msg.ranges[ray])) {
-          continue;
-        }
+    return false;
+  }
 
-        if (scan_msg.ranges[ray] > largest_distance_ray) {
-          largest_distance_ray = scan_msg.ranges[ray];
-          largest_distance_ray_idx = ray;
-        }
+  float calculate_safe_direction() {
+    int largest_distance_ray_idx = 0;
+    float largest_distance_ray = 0.0;
+
+    for (int i = front_ray - front_range; i <= front_ray + front_range; i++) {
+      if (isinf(scan_msg.ranges[i])) {
+        continue;
       }
-      float direction_ = (scan_msg.angle_min +
-                          largest_distance_ray_idx * scan_msg.angle_increment);
 
-      cmd_vel_msg.angular.z = direction_ / 2;
+      if (scan_msg.ranges[i] > largest_distance_ray) {
+        largest_distance_ray = scan_msg.ranges[i];
+        largest_distance_ray_idx = i;
+      }
+    }
+
+    float direction_ = (scan_msg.angle_min +
+                        largest_distance_ray_idx * scan_msg.angle_increment);
+
+    return direction_;
+  }
+
+  void control_loop() {
+    cmd_vel_msg.linear.x = 0.1;
+    cmd_vel_msg.angular.z = 0.0;
+
+    if (is_obstacle_in_front()) {
+      cmd_vel_msg.angular.z = calculate_safe_direction() / 2;
     }
 
     publisher_->publish(cmd_vel_msg);
   }
+
+  int front_range;
+  int front_ray;
+  int front_ray_view;
 
   geometry_msgs::msg::Twist cmd_vel_msg;
   sensor_msgs::msg::LaserScan scan_msg;
@@ -79,6 +85,5 @@ private:
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   rclcpp::spin(make_shared<Patrol>());
-
   rclcpp::shutdown();
 }
